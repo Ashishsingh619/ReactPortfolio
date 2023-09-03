3708896aad45063a73a0c415f04b946f59bf7e26
Ashishsingh619 <ashishdineshsingh@gmail.com>
2023-09-03 16:20:45 +0530

    Added NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import { Link } from 'react-scroll/modules';
+import '../CssFiles/NavBar.css'
+import selfImage from '../Images/SelfImage.jpg';
 function NavBar(){
 
     return (
-        <div>helloworld</div>
+        <div id="NavBar">
+            <nav>
+                <div className="profile">
+                    <img src={selfImage} alt="Image of myself" />
+                    <p className="profileName">Ashish Singh</p>
+                </div>
+                <div className="navItemsContainer">
+                    <Link className="navItems"><p>Home</p></Link>
+                    <Link className="navItems"><p>About</p></Link>
+                    <Link className="navItems"><p>Projects</p></Link>
+                    <Link className="navItems"><p>Contact</p></Link>
+                </div>
+            </nav>
+
+        </div>
     );
 }
 
