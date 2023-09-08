c27fb17823965fba9001f8b74cc92be2f23fa4c2
Ashishsingh619 <ashishdineshsingh@gmail.com>
2023-09-08 22:38:49 +0530

    Added files for about

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -8,10 +8,18 @@ function About(){
         <div className="aboutDetails">
             <p className="aboutMeHeading">ABOUT ME</p>
             <p className="aboutLineBreak"><button></button></p>
-            <p className="aboutMeDescription">Hi, I am Ashish Singh It's Nice to meet you !!</p>
+            <p className="aboutMeDescription">Hi, I am Ashish Singh. It's Nice to meet you !!</p>
             <div className="aboutMeSkillDescription">
-                <div className="aboutDescription"></div>
-                <div className="aboutMeSkills"></div>
+                <div className="aboutDescription">
+                    <p className="aboutDescriptionHeading">Get to know me!</p>
+                    <p className="aboutDescriptionBody">Hello, I am a Software Developer. I am dedicated in the quest of developing solutions that leverage on best practice technologies to deliver over the top user experience. I have a accomplishment history that spans for approximately 2 years in different areas of computing sphere mainly focused on android app development and full stack web development.</p>
+                </div>
+                <div className="aboutMeSkills">
+                    <p className="aboutMeSkillsHeading">My Skills</p>
+                    <div className="aboutMeSkillsItems">
+                        <button className="aboutMeSkillItem">Html</button>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
