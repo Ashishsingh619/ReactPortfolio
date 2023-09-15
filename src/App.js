23621435142a7937f8c11ef445ecdc634ccdae6f
Ashishsingh619 <ashishdineshsingh@gmail.com>
2023-09-15 23:28:59 +0530

    Added file for experience

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import react, { Component } from "react";
 import NavBar from "./Components/NavBar";
 import Home from "./Components/Home";
 import About from "./Components/About"
+import Experience from "./Components/Experience";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <NavBar/>
     <Home/>
     <About/>
+    <Experience/>
     </>
       
   );
---
+++ src/Components/Experience.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../CssFiles/Experience.css"
+
+function Experience(){
+    return(<>
+        <section id="experience">
+            <div className="expDetails">
+                <p className="ExpHeading">Experience</p>
+                    
+            </div>
+        </section>
+    </>);
+}
+
+export default Experience;
